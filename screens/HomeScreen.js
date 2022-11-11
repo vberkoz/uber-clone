dc32343963a3f4034bf61cfa510ac32a1286bdfa
vberkoz <vberkoz@gmail.com>
2022-11-11 11:38:07 +0200

    Implementing Google Autocomplete Library

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,51 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-import React from 'react'
 import tw from 'twrnc'
+import React from 'react'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { StyleSheet, View, SafeAreaView, Image } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
-import NavOptions from '../components/NavOptions';
+import { setDestination, setOrigin } from '../slices/navSlice'
+
+import NavOptions from '../components/NavOptions'
+import { useDispatch } from 'react-redux'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
-          style={{width: 100, height: 100, resizeMode: "contain"}}
-          source={{uri: "https://links.papareact.com/gzs"}}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            }
+          }}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }))
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
         />
 
         <NavOptions />
