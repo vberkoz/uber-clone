5fde5b4d3eee986a94a0ea8c35e5aa3e5f23c2d2
vberkoz <vberkoz@gmail.com>
2022-11-13 10:08:52 +0200

    Complete the NavigateCard Component

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,6 +1,7 @@
 import tw from 'twrnc'
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
@@ -46,6 +47,23 @@ const NavigateCard = () => {
         </View>
 
         <NavFavourites />
+      </View>
+
+      <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('RideOptionsCard')}
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name='car' type='font-awesome' color='white' size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name='fast-food-outline' type='ionicon' color='black' size={16} />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
